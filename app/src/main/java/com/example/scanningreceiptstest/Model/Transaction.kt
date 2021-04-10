8f25adf0be6641defb2eaca807fbda03ac7acb20
Sham-Alsamman <58550633+Sham-Alsamman@users.noreply.github.com>
2021-04-10 22:20:40 +0300

    transactions adapter

--- app/src/main/java/com/example/scanningreceiptstest/Model/Transaction.kt
+++ app/src/main/java/com/example/scanningreceiptstest/Model/Transaction.kt
@@ -5,18 +5,18 @@ import java.util.*
 public abstract class Transaction(
     date: Date,
     amount: Double,
-    notes: String?,
-    category:String,
-    name:String) {
-    var date: Date=date
-    var amount: Double =amount
-    var notes: String?=notes
-    lateinit var id : String
-    var category: String?=category
-    var name: String=name
+    notes: String?, /*******Delete?*******/
+    category: String,
+    name: String
+) {
+    var date: Date = date
+    var amount: Double = amount
+    var notes: String? = notes
+    lateinit var id: String
+    var category: String? = category
+    var name: String = name
 
- fun Add (T: Transaction)
-    {
+    fun Add(T: Transaction) {
 
     }
 
