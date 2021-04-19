a49efefb0ab5876377e4c3369a4c5dab5cdb72dd
Malak <malakkhasawneh99@outlook.com>
2021-04-19 13:49:14 +0300

    lastEdit

--- app/src/main/java/com/example/scanningreceiptstest/Controller/SignUp.kt
+++ app/src/main/java/com/example/scanningreceiptstest/Controller/SignUp.kt
@@ -15,7 +15,7 @@ import kotlinx.android.synthetic.main.activity_sign_up.*
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-class SignUp : NavDrawerActivity () {
+class SignUp : NavDrawerActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,12 +33,10 @@ class SignUp : NavDrawerActivity () {
             if (matcher.find()) {
                 Name.setEndIconDrawable(R.drawable.ic_baseline_error_24)
                 Name.error = "Name Should Contain Only Characters and Numbers"
-            }
-            else if (text.isNullOrEmpty()) {
+            } else if (text.isNullOrEmpty()) {
                 Name.setEndIconDrawable(R.drawable.ic_baseline_error_24)
                 Name.error = "Enter Your Name "
-            }
-            else {
+            } else {
                 Name.setEndIconDrawable(0)
                 Name.error = null
             }
@@ -47,17 +45,15 @@ class SignUp : NavDrawerActivity () {
             if (text.isNullOrEmpty()) {
                 PhoneSignUp.setEndIconDrawable(R.drawable.ic_baseline_error_24)
                 PhoneSignUp.error = "Enter Your Phone Number "
-            }
-            else if(!text.startsWith("+")){
+            } else if (!text.startsWith("+")) {
                 PhoneSignUp.setEndIconDrawable(R.drawable.ic_baseline_error_24)
-                PhoneSignUp.error ="Enter Country code also along with the phone number"
+                PhoneSignUp.error = "Enter Country code also along with the phone number"
             }
-           // to check if the phone number already register in firebase
-            else if (CheckForPhoneNumber(text.toString())){
+            // to check if the phone number already register in firebase
+            else if (CheckForPhoneNumber(text.toString())) {
                 PhoneSignUp.setEndIconDrawable(R.drawable.ic_baseline_error_24)
-                PhoneSignUp.error ="The phone number is already register in the application"
-            }
-            else {
+                PhoneSignUp.error = "The phone number is already register in the application"
+            } else {
                 PhoneSignUp.setEndIconDrawable(0)
                 PhoneSignUp.error = null
             }
@@ -72,11 +68,9 @@ class SignUp : NavDrawerActivity () {
             } else if (text.isNullOrEmpty()) {
                 // PasswordET.setEndIconDrawable(R.drawable.ic_baseline_error_24)
                 pass.error = "Enter Password"
-            }
-            else if(text.length<8){
+            } else if (text.length < 8) {
                 pass.error = "Password must at least 8 characters"
-            }
-            else {
+            } else {
                 //PasswordETSignUp.setEndIconDrawable(0)
                 pass.error = null
             }
@@ -84,14 +78,12 @@ class SignUp : NavDrawerActivity () {
         RePass.editText?.doOnTextChanged { text, start, before, count ->
             val match: String = pass.editText?.text.toString()
             val match2: String = RePass.editText?.text.toString()
-            if(!match2!!.equals(match)){
-                RePass.error="Password are not matching"
-            }
-            else if (text.isNullOrEmpty()) {
+            if (!match2!!.equals(match)) {
+                RePass.error = "Password are not matching"
+            } else if (text.isNullOrEmpty()) {
                 // PasswordET.setEndIconDrawable(R.drawable.ic_baseline_error_24)
                 RePass.error = "Re-type Password"
-            }
-            else {
+            } else {
                 //PasswordETSignUp.setEndIconDrawable(0)
                 RePass.error = null
             }
@@ -106,39 +98,30 @@ class SignUp : NavDrawerActivity () {
     /********************************/
     fun goToVerification(view: View) {
         /***check if all fields are correct..**/
-        if (NameET.editText?.text!!.isEmpty()   && PhoneETSignUp.editText?.text!!.isEmpty() &&  PasswordETSignUp.editText?.text!!.isEmpty() && RepasswordET.editText?.text!!.isEmpty()){
-            Toast.makeText(applicationContext,"Please fill all fields ",Toast.LENGTH_LONG).show()
-        }
-        else if(NameET.editText?.text!!.isEmpty()){
-            Toast.makeText(applicationContext,"Please enter name ",Toast.LENGTH_LONG).show()
-        }
-        else if (PhoneETSignUp.editText?.text!!.isEmpty()){
-            Toast.makeText(applicationContext,"Please enter phone number ",Toast.LENGTH_LONG).show()
-        }
-        else if (PasswordETSignUp.editText?.text!!.isEmpty()){
-            Toast.makeText(applicationContext,"Please enter password ",Toast.LENGTH_LONG).show()
-        }
-        else if (RepasswordET.editText?.text!!.isEmpty()){
-            Toast.makeText(applicationContext,"Please Re-type password ",Toast.LENGTH_LONG).show()
-        }
-        else if(PhoneETSignUp.error!=null){
-            Toast.makeText(applicationContext,PhoneETSignUp.error,Toast.LENGTH_LONG).show()
+        if (NameET.editText?.text!!.isEmpty() && PhoneETSignUp.editText?.text!!.isEmpty() && PasswordETSignUp.editText?.text!!.isEmpty() && RepasswordET.editText?.text!!.isEmpty()) {
+            Toast.makeText(applicationContext, "Please fill all fields ", Toast.LENGTH_LONG).show()
+        } else if (NameET.editText?.text!!.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter name ", Toast.LENGTH_LONG).show()
+        } else if (PhoneETSignUp.editText?.text!!.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter phone number ", Toast.LENGTH_LONG)
+                .show()
+        } else if (PasswordETSignUp.editText?.text!!.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter password ", Toast.LENGTH_LONG).show()
+        } else if (RepasswordET.editText?.text!!.isEmpty()) {
+            Toast.makeText(applicationContext, "Please Re-type password ", Toast.LENGTH_LONG).show()
+        } else if (PhoneETSignUp.error != null) {
+            Toast.makeText(applicationContext, PhoneETSignUp.error, Toast.LENGTH_LONG).show()
 
-        }
-        else if(NameET.error!=null){
-            Toast.makeText(applicationContext,NameET.error,Toast.LENGTH_LONG).show()
+        } else if (NameET.error != null) {
+            Toast.makeText(applicationContext, NameET.error, Toast.LENGTH_LONG).show()
 
-        }
-        else if(PasswordETSignUp.error!=null){
-            Toast.makeText(applicationContext,PasswordETSignUp.error,Toast.LENGTH_LONG).show()
+        } else if (PasswordETSignUp.error != null) {
+            Toast.makeText(applicationContext, PasswordETSignUp.error, Toast.LENGTH_LONG).show()
 
-        }
-        else if(RepasswordET.error!=null){
-            Toast.makeText(applicationContext,RepasswordET.error,Toast.LENGTH_LONG).show()
+        } else if (RepasswordET.error != null) {
+            Toast.makeText(applicationContext, RepasswordET.error, Toast.LENGTH_LONG).show()
 
-        }
-
-       else {
+        } else {
             //open verification page:
             val intent = Intent(applicationContext, Verification::class.java)
             intent.putExtra(PHONE_NUMBER_EXTRA, PhoneETSignUp.editText?.text.toString())
@@ -146,8 +129,9 @@ class SignUp : NavDrawerActivity () {
         }
 
     }
-    fun CheckForPhoneNumber(phone:String): Boolean { // to check if phone number if exist or not in fire base
-    var flag :Boolean =true;
+
+    fun CheckForPhoneNumber(phone: String): Boolean { // to check if phone number if exist or not in fire base
+        var flag: Boolean = true;
         /*
     database.orderByChild("phoneNumber").equalTo(PhoneSignUp).addValueEventListener(object : ValueEventListener {
 
