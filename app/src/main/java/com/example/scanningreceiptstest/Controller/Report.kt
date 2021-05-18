eed83f3b79e251a736590d4c913806416e1111a2
Sham-Alsamman <58550633+Sham-Alsamman@users.noreply.github.com>
2021-05-18 09:04:50 +0300

    preventing rotation

--- app/src/main/java/com/example/scanningreceiptstest/Controller/Report.kt
+++ app/src/main/java/com/example/scanningreceiptstest/Controller/Report.kt
@@ -26,7 +26,7 @@ import kotlin.math.roundToInt
 class Report : NavDrawerActivity(), IFilterSheet {
 
     lateinit var pieChart: PieChart;
-    var valuesList : ArrayList<PieEntry> = ArrayList<PieEntry>();
+    var valuesList: ArrayList<PieEntry> = ArrayList<PieEntry>();
 
     private val filterSheet = BottomSheet_Filter(this)
 
@@ -41,7 +41,7 @@ class Report : NavDrawerActivity(), IFilterSheet {
             filterSheet.show(supportFragmentManager, "BottomSheetDialog")
         }
 
-        val a =  "JD" + CURRENT_USER!!.totalIncome.toString()
+        val a = "JD" + CURRENT_USER!!.totalIncome.toString()
         totalValue.text = a
 
         val b = "JD" + CURRENT_USER!!.remaining.toString()
@@ -53,8 +53,8 @@ class Report : NavDrawerActivity(), IFilterSheet {
         var x = currentDate.get(0)
         var y = currentDate.get(1)
         var today = "$x$y"
-        var res =  (CURRENT_USER!!.totalIncome - CURRENT_USER!!.remaining).div(today.toDouble())
-        val number2digits:Double = (res.toDouble() * 100.0).roundToInt() / 100.0
+        var res = (CURRENT_USER!!.totalIncome - CURRENT_USER!!.remaining).div(today.toDouble())
+        val number2digits: Double = (res.toDouble() * 100.0).roundToInt() / 100.0
 
         val d = "JD" + number2digits.toString() + " / Day"
         avgValue.text = d
@@ -62,14 +62,23 @@ class Report : NavDrawerActivity(), IFilterSheet {
         System.out.println(today)
     }
 
-    fun generate(list : ArrayList<PieEntry>){
+    fun generate(list: ArrayList<PieEntry>) {
 
-        var dataset = PieDataSet(list,".");
+        var dataset = PieDataSet(list, ".");
 
         var pd = PieData(dataset);
         pieChart.data = pd;
 
-        dataset.setColors(Color.CYAN, Color.LTGRAY, Color.BLUE, Color.LTGRAY, Color.DKGRAY, Color.YELLOW, Color.MAGENTA, Color.GRAY)
+        dataset.setColors(
+            Color.CYAN,
+            Color.LTGRAY,
+            Color.BLUE,
+            Color.LTGRAY,
+            Color.DKGRAY,
+            Color.YELLOW,
+            Color.MAGENTA,
+            Color.GRAY
+        )
 
         dataset.setValueTextSize(20f)
         pd.setValueTextSize(20f)
@@ -80,21 +89,20 @@ class Report : NavDrawerActivity(), IFilterSheet {
         pieChart.invalidate()
     }
 
-    fun applyFirstChanges(){
-            Database.getAllExpenses(CURRENT_USER!!.phoneNumber, ::ExpensesDBResult)
+    fun applyFirstChanges() {
+        Database.getAllExpenses(CURRENT_USER!!.phoneNumber, ::ExpensesDBResult)
     }
 
     override fun applyFilterChanges() {
         if (filterSheet.groupFilter == GroupTransactionFilter.Group) {
             for (i in CURRENT_GROUP!!.partners)
                 Database.getAllExpenses(i, ::ExpensesDBResult)
-        }
-        else if (filterSheet.groupFilter == GroupTransactionFilter.Individual) {
+        } else if (filterSheet.groupFilter == GroupTransactionFilter.Individual) {
             Database.getAllExpenses(CURRENT_USER!!.phoneNumber, ::ExpensesDBResult)
         }
     }
 
-    private fun addValues(list: List<Transaction>){
+    private fun addValues(list: List<Transaction>) {
         valuesList = ArrayList<PieEntry>();
 
         var Food = 0.0
@@ -106,44 +114,50 @@ class Report : NavDrawerActivity(), IFilterSheet {
         var Transposition = 0.0
         var Other = 0.0
 
-        for(i in list){
+        for (i in list) {
             i as Expense
-            if( i.category == "Food" )
+            if (i.category == "Food")
                 Food += i.amount
-            else if( i.category == "Shopping")
+            else if (i.category == "Shopping")
                 Shopping += i.amount
-            else if( i.category == "Medical and Health")
+            else if (i.category == "Medical and Health")
                 Medical_and_Health += i.amount
-            else if( i.category == "Education")
+            else if (i.category == "Education")
                 Education += i.amount
-            else if( i.category == "Housing")
+            else if (i.category == "Housing")
                 Housing += i.amount
-            else if( i.category == "Entertainment")
+            else if (i.category == "Entertainment")
                 Entertainment += i.amount
-            else if( i.category == "Transposition")
+            else if (i.category == "Transposition")
                 Transposition += i.amount
-            else if( i.category == "Other")
+            else if (i.category == "Other")
                 Other += i.amount
         }
 
-        var totalPrices = Food.toFloat() + Shopping.toFloat() + Medical_and_Health.toFloat() + Education.toFloat() + Housing.toFloat() + Entertainment.toFloat() + Transposition.toFloat() + Other.toFloat()
+        var totalPrices =
+            Food.toFloat() + Shopping.toFloat() + Medical_and_Health.toFloat() + Education.toFloat() + Housing.toFloat() + Entertainment.toFloat() + Transposition.toFloat() + Other.toFloat()
 
-        if(Food != 0.0)
-            valuesList.add( PieEntry((Food.toFloat()/totalPrices), "Food"))
-        if(Shopping != 0.0)
-            valuesList.add( PieEntry((Shopping.toFloat()/totalPrices), "Shopping"))
-        if(Medical_and_Health != 0.0)
-            valuesList.add( PieEntry((Medical_and_Health.toFloat()/totalPrices), "Medical and Health"))
-        if(Education != 0.0)
-            valuesList.add( PieEntry((Education.toFloat()/totalPrices), "Education"))
-        if(Housing != 0.0)
-            valuesList.add( PieEntry((Housing.toFloat()/totalPrices), "Housing"))
-        if(Entertainment != 0.0)
-            valuesList.add( PieEntry((Entertainment.toFloat()/totalPrices), "Entertainment"))
-        if(Transposition != 0.0)
-            valuesList.add( PieEntry((Transposition.toFloat()/totalPrices), "Transposition"))
-        if(Other != 0.0)
-            valuesList.add( PieEntry((Other.toFloat()/totalPrices), "Other"))
+        if (Food != 0.0)
+            valuesList.add(PieEntry((Food.toFloat() / totalPrices), "Food"))
+        if (Shopping != 0.0)
+            valuesList.add(PieEntry((Shopping.toFloat() / totalPrices), "Shopping"))
+        if (Medical_and_Health != 0.0)
+            valuesList.add(
+                PieEntry(
+                    (Medical_and_Health.toFloat() / totalPrices),
+                    "Medical and Health"
+                )
+            )
+        if (Education != 0.0)
+            valuesList.add(PieEntry((Education.toFloat() / totalPrices), "Education"))
+        if (Housing != 0.0)
+            valuesList.add(PieEntry((Housing.toFloat() / totalPrices), "Housing"))
+        if (Entertainment != 0.0)
+            valuesList.add(PieEntry((Entertainment.toFloat() / totalPrices), "Entertainment"))
+        if (Transposition != 0.0)
+            valuesList.add(PieEntry((Transposition.toFloat() / totalPrices), "Transposition"))
+        if (Other != 0.0)
+            valuesList.add(PieEntry((Other.toFloat() / totalPrices), "Other"))
 
 
         generate(valuesList)
